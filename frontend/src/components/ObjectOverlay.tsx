8994dbc685512435b3254bf7b68064baa5ecd849
Mark-McMahon <mmcmah19@binghamton.edu>
2026-02-21 15:09:54 -0500

    llm analysis embedded collasable

--- frontend/src/components/ObjectOverlay.tsx
+++ frontend/src/components/ObjectOverlay.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef, useEffect, useCallback } from "react";
 import type { TrackedObject } from "../types/index";
 import { useStore } from "../store/useStore";
 import CollapsedPill from "./CollapsedPill";
+import ExpandedCard from "./ExpandedCard";
 
 interface Props {
   obj: TrackedObject;
@@ -10,9 +11,33 @@ interface Props {
 
 function ObjectOverlay({ obj, top }: Props) {
   const toggleExpanded = useStore((s) => s.toggleExpanded);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // Click-outside handler to collapse expanded card
+  const handleClickOutside = useCallback(
+    (e: MouseEvent) => {
+      if (
+        obj.isExpanded &&
+        containerRef.current &&
+        !containerRef.current.contains(e.target as Node)
+      ) {
+        toggleExpanded(obj.trackId);
+      }
+    },
+    [obj.isExpanded, obj.trackId, toggleExpanded]
+  );
+
+  useEffect(() => {
+    if (obj.isExpanded) {
+      document.addEventListener("mousedown", handleClickOutside);
+      return () =>
+        document.removeEventListener("mousedown", handleClickOutside);
+    }
+  }, [obj.isExpanded, handleClickOutside]);
 
   return (
     <div
+      ref={containerRef}
       style={{
         position: "absolute",
         left: obj.smoothedX,
@@ -25,7 +50,16 @@ function ObjectOverlay({ obj, top }: Props) {
       }}
       onClick={() => toggleExpanded(obj.trackId)}
     >
-      <CollapsedPill obj={obj} />
+      {/* Expand/collapse wrapper */}
+      <div
+        style={{
+          overflow: "hidden",
+          maxHeight: obj.isExpanded ? 600 : 40,
+          transition: "max-height 300ms ease-out",
+        }}
+      >
+        {obj.isExpanded ? <ExpandedCard obj={obj} /> : <CollapsedPill obj={obj} />}
+      </div>
     </div>
   );
 }
